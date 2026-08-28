0deff23de9024211433d0ef8bfdb9434fdcabc0c
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-28 20:25:30 +0100

    Remove copyright comments from test.js
    Removed copyright comments from test.js
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- docs/assets/getting-started-assets/javascripts/codemirror/javascript/test.js
+++ docs/assets/getting-started-assets/javascripts/codemirror/javascript/test.js
@@ -1,5 +1,3 @@
-// CodeMirror, copyright (c) by Marijn Haverbeke and others
-// Distributed under an MIT license: http://codemirror.net/LICENSE
 
 ;(function() {
     var mode = CodeMirror.getMode({ indentUnit: 2 }, "javascript")
